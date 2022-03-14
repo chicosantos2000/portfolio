9c5a01af405fbc66a59dc0e340619060b833fff2
Francisco Santos <fingerfranc@gmail.com>
2022-03-14 11:56:54 +0000

    My First React

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,27 @@
+import "./intro.css"
+
+const Intro = () => {
+  return (
+    <div className="i"> 
+        
+        <div className="i-left">
+           <div className="i-left-wrapper">
+              <h2 className="i-intro">Hello, My name is</h2>
+              <h1 className="i-name">Francisco Santos</h1>
+              <div className="i-title"> 
+                <div className="i-tittle-wrapper">
+                  <div className="i-title-item">Self Taught Developer</div>
+                  <div className="i-title-item">Tech Enthusiast</div>
+                  <div className="i-title-item">Petrolhead</div>
+                  <div className="i-title-item">PC Gamer</div>
+                  <div className="i-title-item">Trip Lover</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="i-right">right</div>
+            </div>
+          )
+        }
+
+export default Intro
